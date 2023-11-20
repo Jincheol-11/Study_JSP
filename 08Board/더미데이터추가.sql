2733b72465a6e121b4bcde971f01964dac36da7d
권진철 <sunkiro96@naver.com>
2023-11-20 23:41:29 +0900

    view와 controller의 역할이 합쳐진 MVC1 패턴의 게시판 만들어보기 (더미데이터)

---
+++ 08Board/더미데이터추가.sql
@@ -0,0 +1,10 @@
+INSERT INTO board (title, content, id, postdate, visitcount)
+VALUES ('지금은 봄입니다', '봄의 왈츠', 'musthave',sysdate() ,0);
+INSERT INTO board (title, content, id, postdate, visitcount)
+VALUES ('지금은 여름입니다', '여름의 왈츠', 'musthave',sysdate() ,0);
+INSERT INTO board (title, content, id, postdate, visitcount)
+VALUES ('지금은 가을입니다', '가을의 왈츠', 'musthave',sysdate() ,0);
+INSERT INTO board (title, content, id, postdate, visitcount)
+VALUES ('지금은 겨울입니다', '겨울의 왈츠', 'musthave',sysdate() ,0);
+
+COMMIT;
